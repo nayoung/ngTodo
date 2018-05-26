e0bef39dd78aa2fa5a4075f21c4286cd6e1a4e0f
mia <gndl311@nate.com>
2018-05-26 15:00:39 +0900

    route 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { JqueryComponent } from './jquery/jquery.component';
+import { AngularComponent } from './angular/angular.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const route: Routes = [
+  {path: '', component: IndexComponent, children: [
+      // {path: '', component: HomeComponent},  // / 에 대해서 home이됨
+      {path: 'home', component: HomeComponent},  // /home/
+      {path: 'jquery', component: JqueryComponent}, // /jquery/
+      {path: 'angular', component: AngularComponent} // /angular/
+      ]}
+  // {path: 'admin', component:}. // 관리자등 화면이 다른사이트 구현가능
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HomeComponent,
+    JqueryComponent,
+    AngularComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(route),
   ],
   providers: [],
   bootstrap: [AppComponent]
