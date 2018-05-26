cbb6d832082aaa60624a8707593d48edc7d55fd0
mia <gndl311@nate.com>
2018-05-26 15:53:51 +0900

    메뉴생성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,16 @@ import { HomeComponent } from './home/home.component';
 import { JqueryComponent } from './jquery/jquery.component';
 import { AngularComponent } from './angular/angular.component';
 import {RouterModule, Routes} from '@angular/router';
+import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
 
 const route: Routes = [
   {path: '', component: IndexComponent, children: [
-      // {path: '', component: HomeComponent},  // / 에 대해서 home이됨
-      {path: 'home', component: HomeComponent},  // /home/
+      {path: '', component: HomeComponent},  // / 에 대해서 home이됨
+      // {path: 'home', component: HomeComponent},  // /home/
       {path: 'jquery', component: JqueryComponent}, // /jquery/
       {path: 'angular', component: AngularComponent} // /angular/
       ]}
-  // {path: 'admin', component:}. // 관리자등 화면이 다른사이트 구현가능
+  // { path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule'}
 ];
 
 @NgModule({
@@ -29,6 +30,10 @@ const route: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(route),
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
